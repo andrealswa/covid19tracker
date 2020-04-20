222a6f324aedc4d544a4319e77c91a3c423d6393
andrealswa <swartz8@uwindsor.ca>
2020-04-20 15:54:10 -0400

    new filter

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { NativeSelect, FormControl } from '@material-ui/core';
+import SelectSearch from 'react-select-search';
 
 import styles from './CountryPicker.module.css';
+import './selectSearch.css';
 
 import { fetchCountries } from '../../api';
 
@@ -16,20 +18,35 @@ const CountryPicker = ({ handleCountryChange }) => {
     getCountries();
   }, [setFetchedCountries]);
 
+  const options = fetchedCountries.map((country) => ({
+    name: country,
+    value: country,
+  }));
+
   return (
-    <FormControl className={styles.formControl}>
-      <NativeSelect
-        defaultValue=""
-        onChange={(e) => handleCountryChange(e.target.value)}
-      >
-        <option value="">Global</option>
-        {fetchedCountries.map((country, i) => (
-          <option key={i} value={country}>
-            {country}
-          </option>
-        ))}
-      </NativeSelect>
-    </FormControl>
+    <div>
+      <SelectSearch
+        options={options}
+        defaultValue="sv"
+        name="country"
+        placeholder="Choose your country"
+        autoComplete="on"
+        search={true}
+      />
+      <FormControl className={styles.formControl}>
+        <NativeSelect
+          defaultValue=""
+          onChange={(e) => handleCountryChange(e.target.value)}
+        >
+          <option value="">Global</option>
+          {fetchedCountries.map((country, i) => (
+            <option key={i} value={country}>
+              {country}
+            </option>
+          ))}
+        </NativeSelect>
+      </FormControl>
+    </div>
   );
 };
 
